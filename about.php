301584145475b7f3fbc5ef656ceebd6eeac2780b
alstdu <lexi.dugo@gmail.com>
2024-02-29 15:05:50 -0800

    Add basic information for the about me section

--- about.php
+++ about.php
@@ -10,5 +10,23 @@
     <header>
         <?php include_once("menu.html"); ?>
     </header>
+    <h1>About Me</h1>
+    <img class="portrait">
+    <main class='glass-effect'>
+        <div class='about-me'>
+            <p>School: British Colombia Institute of Technology - New Media Design and Web Development</p>
+            <p>Location: New Westminster, BC</p>
+            <p>Hobbies: Video Games, Yoga, Running my bubble tea Instagram page</p>
+            <p>D&D Homebrew: Dryad</p>
+            <p>Hidden Talent: Able to recognize many spoken languages</p>
+        </div>
+        <h2>Let's Connect</h2>
+        <div class="lets-connect">
+            <div class="icons">
+            </div>
+            <div class="handles">
+            </div>
+        </div>
+    </main>
 </body>
 </html>
