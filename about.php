c38798fb3b2468720f8e4bc1ffd727bc3e6a6f5c
alstdu <lexi.dugo@gmail.com>
2024-03-06 13:30:52 -0800

    Optimize html for desktop styling

--- about.php
+++ about.php
@@ -10,25 +10,29 @@
     <header>
         <?php include_once("menu.html"); ?>
     </header>
-    <h1>About Me</h1>
-    <img class="portrait">
-    <main class='glass-effect'>
-        <div class='about-me'>
-            <p>School: British Colombia Institute of Technology - New Media Design and Web Development</p>
-            <p>Location: New Westminster, BC</p>
-            <p>Hobbies: Video Games, Yoga, Running my bubble tea Instagram page</p>
-            <p>D&D Homebrew: Dryad</p>
-            <p>Hidden Talent: Able to recognize many spoken languages</p>
-        </div>
-        <h2>Let's Connect</h2>
-        <div class="lets-connect">
-            <div class="social-icons">
+    <div class="desktop">
+        <img class="portrait" src="./assets/images/BCIT DT-10.jpg">
+        <main class='glass-effect'>
+            <div class='about-me'>
+            <h1>About Me</h1>
+                <p>School: British Colombia Institute of Technology - New Media Design and Web Development</p>
+                <p>Location: New Westminster, BC</p>
+                <p>Hobbies: Video Games, Yoga, Running my bubble tea Instagram page</p>
+                <p>D&D Homebrew: Dryad</p>
+                <p>Hidden Talent: Able to recognize many spoken languages</p>
             </div>
-            <div class="handles">
+            <div class="lets-connect">
+                <h2>Let's Connect</h2>
+                <div class="social-icons">
+                </div>
+                <div class="handles">
+                </div>
             </div>
-        </div>
-    </main>
-    <p>Think I'd be a good fit for you or a friend? Download my resume!</p>
-    <button class="download-button">Download</button>
+        </main>
+    </div>
+    <div class="resume">
+        <p>Think I'd be a good fit for you or a friend? Download my resume!</p>
+        <button class="download-button">Download</button>
+    </div>
 </body>
 </html>
