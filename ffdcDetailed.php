2f44121e1e92eb2917dce3c73200001be4b45bce
alstdu <lexi.dugo@gmail.com>
2024-03-11 16:02:32 -0700

    Initialize furry friends carecare detailed proj page

---
+++ ffdcDetailed.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Furry Friends Daycare Project</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header>
+    <?php include_once("backtoprojects.html"); ?>
+    </header>
+    <div class="artifact-keywords-container">
+        <div class="artifact-keywords">CSS</div>
+        <div class="artifact-keywords">SEO</div>
+        <div class="artifact-keywords">Accessibility</div>
+    </div>
+    <h1>Furry Friends Daycare</h1>
+    <img src="">
+    <div class="glass-effect artifact-stats">
+        <p><span class="artifact-stats-headers">Time:</span> blah blah</p>
+        <p><span class="artifact-stats-headers">Languages:</span>nlsldhajl</p>
+        <p><span class="artifact-stats-headers">Skills:</span>aslkjdla</p>
+        <p><span class="artifact-stats-headers">Contributors:</span>alskdjla</p>
+    </div>
+    <div class="glass-effect artifact-process">
+        <h2>Process</h2>
+        <p></p>
+        <figure>
+            <img src="">
+            <figcaption></figcaption>
+        </figure>
+        <p></p>
+        <figure>
+            <img src="">
+            <figcaption></figcaption>
+        </figure>
+        <p></p>
+        <figure>
+            <img src="">
+            <figcaption></figcaption>
+        </figure>
+    </div>
+    <div class="glass-effect artifact-challenges">
+        <h2>Challenges & Solutions</h2>
+        <p></p>
+        <p></p>
+        <figure>
+            <img src="">
+            <figcaption></figcaption>
+        </figure>
+        <p></p>
+    </div>
+    <div class="glass-effect artifact-extras">
+        <h2>Post-Coutse Additions</h2>
+        <p></p>
+        <figure>
+                <img src="">
+                <figcaption></figcaption>
+        </figure>
+        <p></p>
+    </div>
+    <div class="artifact-buttons-container">
+        <button class="artifact-buttons">Sheets</button>
+        <button class="artifact-buttons">View Code</button>
+        <button class="artifact-buttons">Live Site</button>
+    </div>
+</body>
+</html>
