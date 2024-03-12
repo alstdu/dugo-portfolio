353a29251006449ba87d84cf54681edc1727d4e9
alstdu <lexi.dugo@gmail.com>
2024-03-11 20:05:30 -0700

    Style sections of project pages

--- ffdcDetailed.php
+++ ffdcDetailed.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Furry Friends Daycare Project</title>
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/projectDetailed.css">
 </head>
 <body>
     <header>
@@ -17,13 +18,13 @@
     </div>
     <h1>Furry Friends Daycare</h1>
     <img src="">
-    <div class="glass-effect artifact-stats">
-        <p><span class="artifact-stats-headers">Time:</span>~17 Hours (my contribution)</p>
-        <p><span class="artifact-stats-headers">Languages:</span>HTML, CSS, JS</p>
-        <p><span class="artifact-stats-headers">Skills:</span>Scrum Methodology, Accessible Design, GitHub Collaboration, Designing from a Wireframe, SEO Markup</p>
-        <p><span class="artifact-stats-headers">Contributors:</span><span class="contributors">Manh Ellis</span> (Copywriter & Development Assistant), <span class="contributors">Kimia Ashrafi</span> (Social Icons & Product Owner), <span class="contributors">Myta Palo</span> (Branding), <span>Stella Park</span> (Scrum Master & UX/UI)</p>
+    <div class="glass-effect section artifact-stats">
+        <p><span class="artifact-stats-headers">Time:</span> ~17 Hours (my contribution)</p>
+        <p><span class="artifact-stats-headers">Languages:</span> HTML, CSS, JS</p>
+        <p><span class="artifact-stats-headers">Skills:</span> Scrum Methodology, Accessible Design, GitHub Collaboration, Designing from a Wireframe, SEO Markup</p>
+        <p><span class="artifact-stats-headers">Contributors:</span><span class="contributors"> Manh Ellis</span> (Copywriter & Development Assistant), <span class="contributors">Kimia Ashrafi</span> (Social Icons & Product Owner), <span class="contributors">Myta Palo</span> (Branding), <span class="contributors">Stella Park</span> (Scrum Master & UX/UI)</p>
     </div>
-    <div class="glass-effect artifact-process">
+    <div class="glass-effect section artifact-process">
         <h2>Process</h2>
         <p>For my project management course, we were tasked with coming up with a fictional business and creating branding, wireframes, and a website using the scrum methodology.  The whole project was done using a 5 week sprint structure. I created the template for our All In One Google Sheet which included time logs, our backlog, sprints, and burndown charts.</p>
         <figure>
@@ -41,7 +42,7 @@
             <figcaption>To see this page, <a>tap here</a></figcaption>
         </figure>
     </div>
-    <div class="glass-effect artifact-challenges">
+    <div class="glass-effect section artifact-challenges">
         <h2>Challenges & Solutions</h2>
         <p>Most of the challenges I encountered were related to making certain elements accessible. This was the first project in which I took a deep dive into accessible elements.</p>
         <p>I used many resources to help guide decision making such as <a>Aditus’ guide on accessible accordion menus</a>, <a>MDN’s documentation</a> on forms and inputs, and multiple sources for how to style radio buttons without the checkbox.</p>
@@ -51,7 +52,7 @@
         </figure>
         <p>You can check out <a>my GitHub repo’s</a> README to see more sources.</p>
     </div>
-    <div class="glass-effect artifact-extras">
+    <div class="glass-effect section artifact-extras">
         <h2>Post-Coutse Additions</h2>
         <p>After the course was completed, I added some SEO markup to the HTML to practice implementing structured data markup and metadata.</p>
         <figure>
@@ -66,4 +67,5 @@
         <button class="artifact-buttons">Live Site</button>
     </div>
 </body>
+<?php include_once("backtothetop.html"); ?>
 </html>
