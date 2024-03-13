f9d96d8efbbd33ec1f75c00dace4e4195c25f69b
Lexi Dugo <lexidugo@gmail.com>
2024-03-13 11:23:07 -0700

    Initialize project listings page

---
+++ projectList.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Projects</title>
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header>
+        <?php include_once("menu.html"); ?>
+    </header>
+    <div class="artifact-listings">
+        <div class="artifact-listing">testing</div>
+        <div class="artifact-listing">test</div>
+        <div class="artifact-listing">trdy</div>
+        <div class="artifact-listing">yrdy</div>
+    </div>
+
+</body>
+</html>
