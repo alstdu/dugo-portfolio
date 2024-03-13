d08133b513587b3768f6c9e4d1d457c045c5d4c7
Lexi Dugo <lexidugo@gmail.com>
2024-03-13 11:31:20 -0700

    Enable smooth scrolling on homepage

--- index.php
+++ index.php
@@ -5,10 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Home</title>
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/homepageStyles.css">
 </head>
 <body>
     <header>
-        <?php include_once("menu.html"); ?>
+        <nav>
+            <img class="logo" src="./assets/images/logo.svg">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="#mySkills">My Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="./about.php">About Me</a></li>
+            </ul>
+        </nav>
     </header>
+    <section id="home">
+    <h2>Big FU 3D element Placeholder</h2>
+    </section>
+    <section id="projects">
+    <h2>My Projects</h2>
+    </section>
+    <section id="mySkills">
+    <h2>Skills</h2>
+    </section>
 </body>
 </html>
