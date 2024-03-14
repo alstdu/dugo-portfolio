32a8e53380ba5e15340b650843be9084311888bd
alstdu <lexi.dugo@gmail.com>
2024-03-14 12:19:11 -0700

    Initialize code for spline

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,9 @@
-import { Application } from '@splinetool/runtime';
+import {Application} from '@splinetool/runtime';
 
-const canvas = document.getElementById('canvas3d');
+const canvas = document.getElementById( 'canvas3d' );
 
 // start the application and load the scene
-const spline = new Application(canvas);
-spline.load('spline/scene.splinecode');
+const spline = new Application( canvas );
+spline.load( 'spline/scene.splinecode' );
+
+console.log( 'hello' );
