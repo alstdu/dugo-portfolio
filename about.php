b887fc4a78a8d630318f462f15dbddc19d72482a
alstdu <lexi.dugo@gmail.com>
2024-03-18 11:04:12 -0700

    Add a content wraper for future styling

--- about.php
+++ about.php
@@ -10,37 +10,39 @@
     <header>
         <?php include_once("menu.html"); ?>
     </header>
-    <div class="desktop">
-        <img class="portrait" src="./assets/images/BCIT DT-10.png">
-        <main class='glass-effect'>
-            <div class='about-me'>
-            <h1>About Me</h1>
-                <p>School: British Colombia Institute of Technology - New Media Design and Web Development</p>
-                <p>Location: New Westminster, BC</p>
-                <p>Hobbies: Video Games, Yoga, Running my bubble tea Instagram page</p>
-                <p>D&D Homebrew: Dryad</p>
-                <p>Languages: English - Native | French - Beginner | Mandarin - Beginner</p>
-            </div>
-            <div class="lets-connect">
-                <h2>Let's Connect</h2>
-                <div class="connect-columns">
-                    <div class="social-icons">
-                        <img src="./assets/images/linkedinlogo.svg">
-                        <img src="./assets/images/instagramlogo.svg">
-                        <img src="./assets/images/githublogo.svg">
-                    </div>
-                    <div class="handles">
-                        <p>Lexi Dugo</p>
-                        <p>lexidugo</p>
-                        <p>alstdu</p>
+    <div class="content-wrapper">
+        <div class="desktop">
+            <img class="portrait" src="./assets/images/BCIT DT-10.png">
+            <main class='glass-effect'>
+                <div class='about-me'>
+                <h1>About Me</h1>
+                    <p>School: British Colombia Institute of Technology - New Media Design and Web Development</p>
+                    <p>Location: New Westminster, BC</p>
+                    <p>Hobbies: Video Games, Yoga, Running my bubble tea Instagram page</p>
+                    <p>D&D Homebrew: Dryad</p>
+                    <p>Languages: English - Native | French - Beginner | Mandarin - Beginner</p>
+                </div>
+                <div class="lets-connect">
+                    <h2>Let's Connect</h2>
+                    <div class="connect-columns">
+                        <div class="social-icons">
+                            <img src="./assets/images/linkedinlogo.svg">
+                            <img src="./assets/images/instagramlogo.svg">
+                            <img src="./assets/images/githublogo.svg">
+                        </div>
+                        <div class="handles">
+                            <p>Lexi Dugo</p>
+                            <p>lexidugo</p>
+                            <p>alstdu</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
-    </div>
-    <div class="resume">
-        <p>Think I'd be a good fit for you or a friend? Download my resume!</p>
-        <button class="download-button" id="downloadButton"><a href="https://profile.indeed.com/p/alexisd-fdpr3c4">Download</a></button>
+            </main>
+        </div>
+        <div class="resume">
+            <p>Think I'd be a good fit for you or a friend? Download my resume!</p>
+            <button class="download-button" id="downloadButton"><a href="https://profile.indeed.com/p/alexisd-fdpr3c4">Download</a></button>
+        </div>
     </div>
     <script src="./scripts/scripts.js"></script>
     <script src="./scripts/print.js"></script>
