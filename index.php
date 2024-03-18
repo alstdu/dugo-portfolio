0abad366a834b96d2fc00ee59ca0dfb38ff2e4b7
alstdu <lexi.dugo@gmail.com>
2024-03-18 11:03:59 -0700

    Add drop shadow and hover effects

--- index.php
+++ index.php
@@ -36,35 +36,35 @@
     <section id="projects">
         <h2>Projects</h2>
         <div id="dndSection">
-            <img src="./assets/images/dndtools-preview.svg" alt="preview of D&D Tools Project">
+            <img class="dndtools-preview" src="./assets/images/dndtools-preview.svg" alt="preview of D&D Tools Project">
             <h3>D&D Tools</h3>
             <p>A collection of D&D tools including a spells table, a point buy calculator and a 3D dice roller. Created using HTML, CSS, JS & packaged with Webpack.
             <br>
-            <span class="arrow-icon">&#8594;</span> <a href="">Check It Out</a>
+            <span class="arrow-icon-dndtools">&#8594; <a href="">Check It Out</a></span>
             </p>
         </div>
         <div id="ilogSection">
-            <img src="./assets/images/ilog-preview.svg" alt="preview of ILOG ELD Project">
+            <img class="ilog-preview" src="./assets/images/ilog-preview.svg" alt="preview of ILOG ELD Project">
             <h3>ILog ELD</h3>
             <p>ILog ELD is an HOS (hours of service) ELD (electronic logging device) for owner operators. It features the ability to add, edit, and certify logs.
             <br>
-            <span class="arrow-icon">&#8594;</span> <a href="">Check It Out</a>
+            <span class="arrow-icon-ilog">&#8594; <a href="">Check It Out</a></span> 
             </p>
         </div>
         <div id="onlineTarotSection">
-            <img src="./assets/images/onlinetarot-preview.svg" alt="preview of Online Tarot Card Project">
+            <img class="onlinetarot-preview" src="./assets/images/onlinetarot-preview.svg" alt="preview of Online Tarot Card Project">
             <h3>Online Tarot</h3>
             <p>An online tarot card picker that allows you to do one, two, and three card spreads. It features a deck that I created myself, and shows keywords for each.
             <br>
-            <span class="arrow-icon">&#8594;</span> <a href="">Check It Out</a>
+            <span class="arrow-icon-onlinetarot">&#8594; <a href="">Check It Out</a></span> 
             </p>
         </div>
         <div id="ffdSection">
-            <img src="./assets/images/ffdc-preview.svg" alt="preview of Furry Friends Daycare project">
+            <img class="ffdc-preview" src="./assets/images/ffdc-preview.svg" alt="preview of Furry Friends Daycare project">
             <h3>Furry Friends Daycare</h3>
             <p>Furry Friends Daycare is a fictional pet sitting business. Our job was to create the brand, create the wireframes, and develop the website using the Agile Methodology.
             <br>
-            <span class="arrow-icon">&#8594;</span> <a href="./ffdcDetailed.php">Check It Out</a>
+            <span class="arrow-icon-ffdc">&#8594; <a href="./ffdcDetailed.php">Check It Out</a></span>
             </p>
         </div>
         <div class="gradient-border">
