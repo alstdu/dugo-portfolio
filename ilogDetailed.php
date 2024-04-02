0f93e663c66011f0f227108ac8bca5cd31ac35ae
alstdu <lexi.dugo@gmail.com>
2024-04-01 20:04:38 -0700

    initialize artifact pages

---
+++ ilogDetailed.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ILog ELD</title>
+    <link rel="stylesheet" href="./css/backtoprojects.css">
+    <link rel="stylesheet" href="./css/backtotop.css">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/projectDetailed.css">
+</head>
+<body>
+    <header>
+    <?php include_once("backtoprojects.html"); ?>
+    </header>
+    <div class="artifact-keywords-container">
+        <div class="gradient-border">
+            <div class="artifact-keywords">Figma</div>
+        </div>
+        <div class="gradient-border">
+            <div class="artifact-keywords">UI Design</div>
+        </div>
+        <div class="gradient-border">
+            <div class="artifact-keywords">User Testing</div>
+        </div>
+    </div>
+    <div class="artifact-header">
+        <div class="artifact-image-title">
+            <h1>ILog ELD</h1>
+            <img src="./assets/images/ilog-preview.svg">
+        </div>
+        <div class="glass-effect artifact-stats">
+            <p><span class="artifact-stats-headers">Time:</span> ~25 Hours</p>
+            <p><span class="artifact-stats-headers">Programs:</span> Figma - Prototyping</p>
+            <p><span class="artifact-stats-headers">Skills:</span> UI Design, User Research, User Testing, Figma, Figma Prototyping, User Stories & Scenarios, Empathy Mapping</p>
+        </div>
+    </div>
+    <h2>Process V.1</h2>
+    <div class="glass-effect section artifact-section artifact-process">
+        <p>I knew I wanted to do an ELD project because it is the type of software my partner and in-laws create for their business. I thought it would be a great way to challenge myself to do something outside the norm of what other apps people are creating. It also helps to have someone review my work to make sure the features and layout make sense to the end user. I hope to create a working prototype of this app next term in the app development class. There are still a few features that I don’t know how to implement in Figma that I hope to implement when I build the app next term.</p>
+        <figure>
+            <img src="./assets/images/artifact-photos/ffdc-1.png">
+            <figcaption>User Persona, Michael</figcaption>
+        </figure>
+        <p>I started out by creating a User Persona that I could use to guide the creation of my app. 
+            I came up with Michael, 52, who is an owner operator in BC. Frankly, 
+            he wishes drivers were still using paper and just wants an app that does the job and is easy to navigate.
+            I also created an empathy map and journey map before beginning the User Flow. 
+        </p>
+        <figure>
+            <img src="./assets/images/artifact-photos/ffdc-2.png">
+            <figcaption>User Flow</figcaption>
+        </figure>
+        <p>Once my user flow was completed, I was able to start my wireframes. At the beginning of this project, I was still
+            relatively new to Figma, so I opted for hand-drawing my wireframes as a fold out to illustrate the process.
+        </p>
+        <figure>
+            <img src="./assets/images/artifact-photos/ffdc-3.png">
+            <figcaption>Wireframes</figcaption>
+        </figure>
+        <p>I then moved over to Figma to complete my prototype. I did three iterations of my prototype. After the first iteration was complete,
+            I moved on to user testing. The user testing was conducted using Maze, and the results were analyzed to see areas of improvement. 
+            If you'd like to check out my project report detailing the user testing and results, please head to the bottom of this page!
+            After getting my test results back, I improved the prototype once more and after the course was over, my professor provided
+            additional feedback which I also implemented.
+        </p>
+        <figure>
+            <img src="./assets/images/artifact-photos/ffdc-3.png">
+            <figcaption>Dashboard screen</figcaption>
+        </figure>
+    </div>
+    <h2>Challenges & Solutions</h2>
+    <div class="glass-effect section artifact-section artifact-challenges">
+       <p>The main issues I had were in the testing phase. As I illustrate in my report, 
+        the first issue I encountered was that mostly every person tested my prototype on mobile. 
+        Maze, unfortunately, is not very mobile friendly. I let my testers know to use desktop if possible, 
+        but it’s just more convenient to test on mobile if that’s how you’re being sent the test.
+         One big downside to this is that the user doesn’t know they need to scroll down to see everything, 
+         and hotspots do not appear if they mis-click. In my next test, I made sure to put some more thought 
+         into my starting screens to make it more like a process flow versus making them start from 
+         the beginning every time with no guidance. </p>
+        <figure class="figure-row-span">
+            <img src="./assets/images/artifact-photos/ffdc-4.png">
+            <figcaption>Issues With Testing</figcaption>
+        </figure>
+        <p>The second issue I encountered was more of an oversight, which is that I wasn't aware that my classmates
+            would be the main ones testing my prototype. Originally, I had people in mind who were already familiar with
+            the software to avoid the need to explain how to use it. It's reasonable to say my classmates have likely
+            never even heard of an ELD prior to this. I did, however; end up sending the test to said people along with my classmates.
+         </p>
+    </div>
+    <h2>Post-Course Additions</h2>
+    <div class="glass-effect section artifact-section artifact-extras">
+        <p>After the course was completed, I added some SEO markup to the HTML to practice implementing structured data markup and metadata.</p>
+        <figure class="figure-row-span">
+                <img src="./assets/images/artifact-photos/ffdc-5.png">
+                <figcaption>Structured Data Markup</figcaption>
+        </figure>
+        <p>Our New Media Marketing class taught us about the importance of SEO and how crawlers search webpages for different types of data. We also learned how to make a marketing plan from start to finish.</p>
+    </div>
+    <div class="artifact-buttons-container">
+        <button class="artifact-buttons"><a href="https://www.canva.com/design/DAFvfrS4J3o/WU0MQfaqAybuFiGaCly3wA/view?utm_content=DAFvfrS4J3o&utm_campaign=designshare&utm_medium=link&utm_source=editor" target="_blank">Report</a></button>
+        <button class="artifact-buttons"><a href="https://www.figma.com/proto/s130zms78A6kuSolnLWPo5/Phase-4?type=design&t=IHIrghTPbOQjJCOP-1&scaling=scale-down&page-id=0%3A1&node-id=1-4&starting-point-node-id=1%3A4&mode=design" target="_blank">View Prototype</a></button>
+        <button class="artifact-buttons"><a href="https://furryfriendsdaycare.lexidugo.com" target="_blank">Live Site</a></button>
+    </div>
+    <?php include_once("backtothetop.html"); ?>
+    <script src="./scripts/projectDetailed.js"></script>
+</body>
+</html>
